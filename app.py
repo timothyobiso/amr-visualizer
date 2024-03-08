b25b299ea2058e3bdf6f1b7552ed629a657f93e6
Timothy Obiso <9258849+timothyobiso@users.noreply.github.com>
2024-03-08 15:30:06 -0500

    v1

---
+++ app.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+import graphviz
+from collections import Counter
+
+
+def parse_amr(amr):
+    graph = graphviz.Digraph()
+    amr = amr.replace("(", " ( ").replace(")", " ) ").replace(' / ', '/')
+    tokens = amr.split()
+    stack = []
+    nodes = {}
+    layer = -1
+    waiting = []
+    parent = []
+    for token in tokens:
+
+        if token == "(":
+            stack.append(token)
+        elif token == ")":
+            stack.pop()
+            parent = parent[:len(parent)-1]
+        elif token.startswith(':'):
+            print("PARENT", parent)
+            waiting.append((parent[-1], token))
+        else:
+
+            if waiting:
+                for p, e in waiting:
+                    if "/" not in token:
+                        token = nodes[token]
+                    graph.edge(p, token, label=e)
+                waiting = []
+            nodes[token.split("/")[0]] = token
+            if "/" in token:
+                graph.node(token)
+                parent.append(token)
+
+    return graph
+
+
+st.markdown("# AMR Visualizer")
+text = st.text_area("Enter an AMR graph", height=150, value="(a / and :op1 (b / big) :op2 (c / cat))")
+
+st.graphviz_chart(parse_amr(text))
+
+
+
